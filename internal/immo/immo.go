41bbca01fc0457e1b495f61afc1126a21bad749a
Reza Zarin <rezazarindast76@gmail.com>
2024-05-22 17:53:43 +0330

    :tada: Using SQL database instead of json

--- internal/immo/immo.go
+++ internal/immo/immo.go
@@ -2,10 +2,11 @@ package immo
 
 import (
 	pb "Immortals/api/gRPC/immo/immo"
-	db "Immortals/internal/database"
+	db "Immortals/internal/database/sqlite/sqlc"
 	"Immortals/pkg/models"
-	"Immortals/pkg/node"
 	"context"
+	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"net"
@@ -13,58 +14,91 @@ import (
 	"google.golang.org/grpc"
 )
 
-type server struct {
+type Server struct {
 	pb.ImmoServiceServer
-	db.DbContext
+	store *db.Store
 }
 
-func (s *server) DiscoverNode(ctx context.Context, req *pb.NodeRequest) (*pb.NodeResponse, error) {
+var ErrNodeExists = errors.New("node already exists")
+
+func (s *Server) DiscoverNode(ctx context.Context, req *pb.NodeRequest) (*pb.NodeResponse, error) {
 	log.Printf("Discovering node with address %s\n", req.Address)
-	nodeData, err := node.Discover(req.Name, req.Address)
+	nodeData, err := db.Discover(req.Name, req.Address)
 	if err != nil {
 		log.Printf("Error Discovering node with address %s\n : %s", req.Address, err)
 		return nil, err
 	}
 	nodeResponse := &pb.NodeResponse{}
-	nodeResponse.Id = nodeData.NodeID
-	nodeResponse.Sensor = DiscoverSensor(nodeData.Sensor)
-	nodeResponse.Actuator = DiscoverActuator(nodeData.Actuator)
+	nodeResponse.Name = nodeData.Name.String
+	// nodeResponse.Sensor = DiscoverSensor(nodeData.Sensor)
+	// nodeResponse.Actuator = DiscoverActuator(nodeData.Actuator)
 	fmt.Println("node Data:", nodeData)
 
 	return nodeResponse, nil
 }
 
-func (s *server) AddNode(ctx context.Context, req *pb.NodeRequest) (*pb.NodeResponse, error) {
+func (s *Server) AddNode(ctx context.Context, req *pb.NodeRequest) (*pb.NodeResponse, error) {
 	log.Printf("Adding node with address %s and name %s\n", req.Address, req.Name)
-	nodeData, err := node.Add(s.DbContext, req.Name, req.Address)
+	node, err := s.store.GetNodeByClientID(context.Background(), sql.NullString{String: req.Address, Valid: true})
+
+	if err != nil {
+		fmt.Printf("Error Adding Node: %s", err)
+	}
+
+	if node.ID > 0 {
+		fmt.Println("Node is Already Exists")
+		return nil, ErrNodeExists
+	}
+
+	_, err = s.DiscoverNode(context.Background(), req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil, err
+	}
+
+	arg := db.CreateNodeParams{
+		Name:     sql.NullString{String: req.Name, Valid: true},
+		ClientID: sql.NullString{String: req.Address, Valid: true},
+	}
+
+	node, err = s.store.CreateNode(context.Background(), arg)
 	if err != nil {
 		return nil, err
 	}
+
 	return &pb.NodeResponse{
-		Id:       nodeData.NodeID,
-		Sensor:   DiscoverSensor(nodeData.Sensor),
-		Actuator: DiscoverActuator(nodeData.Actuator),
-		Error:    ""}, nil
+		Id:    node.ID,
+		Error: ""}, nil
 }
 
-func (s *server) ListNodes(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
+func (s *Server) ListNodes(ctx context.Context, req *pb.ListRequest) (*pb.ListResponse, error) {
 	listResponse := &pb.ListResponse{}
 
-	nodes, err := node.List(s.DbContext)
+	nodes, err := s.store.ListNodes(context.Background())
 	if err != nil {
 		return nil, err
 	}
-	for _, m := range nodes {
+	for _, node := range nodes {
 		nodeResponse := &pb.NodeResponse{}
-		nodeResponse.Name = m.NodeName
-		nodeResponse.Id = m.NodeID
-		nodeResponse.Name = m.NodeName
-		nodeResponse.Sensor = DiscoverSensor(m.Sensor)
-		nodeResponse.Actuator = DiscoverActuator(m.Actuator)
+		nodeResponse.Id = node.ID
+		nodeResponse.Name = node.Name.String
 		listResponse.Nodes = append(listResponse.Nodes, nodeResponse)
 	}
-	fmt.Println("Nodes : ")
+	fmt.Printf("Nodes : %v\n", nodes)
 	return listResponse, nil
+}
+
+func (s *Server) RemoveNode(ctx context.Context, req *pb.NodeRequest) (*pb.RemoveResponse, error) {
+	removeResponse := &pb.RemoveResponse{}
+
+	err := s.store.DeleteNodeByClientID(context.Background(), sql.NullString{String: req.Address, Valid: true})
+	if err != nil {
+		log.Printf("Error in deleting node : %s", err)
+		return nil, err
+	}
+	fmt.Printf("Node Removed %s\n", req.Address)
+	removeResponse.Status = true
+	return removeResponse, nil
 }
 
 func DiscoverSensor(sensors []models.Sensor) []*pb.SensorResponse {
@@ -89,13 +123,13 @@ func DiscoverActuator(actuators []models.Actuator) []*pb.ActuatorResponse {
 	return actuatorList
 }
 
-func SetupImmo(dbContext db.DbContext) {
+func SetupImmo(store *db.Store) {
 	lis, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		log.Fatalf("failed to listen : %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterImmoServiceServer(s, &server{DbContext: dbContext})
+	pb.RegisterImmoServiceServer(s, &Server{store: store})
 
 	log.Println("Immo server started: localhost:50051")
 	if err := s.Serve(lis); err != nil {
